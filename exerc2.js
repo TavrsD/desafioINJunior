4dbf81bf3fed38dc86731add7d534699d666cdd0
Daniel <dtavares@id.uff.br>
2025-01-17 01:19:26 -0300

    feat:resolucao do exercicio 2

--- exerc2.js
+++ exerc2.js
@@ -11,3 +11,33 @@ inteiros positivos são aceitos. Se o usuário digitar um número
 inválido, exiba uma mensagem de erro e peça a entrada
 novamente.
 */
+let keep = false;
+
+do {
+    const num = parseInt(prompt(`Digite um inteiro para calcular o fatorial`), 10);
+    
+    if (num <= 0 || isNaN(num)) {
+        alert(`entrada inválida.`);
+    } else {
+        let result = num;
+        for (i = num-1; i > 0; i--) {
+            result = result*i;
+        }
+        alert(`O fatorial de ${num} é ${result}`);
+    }
+    
+    let keepAsking = true;
+    do {
+        let ask = prompt(`Deseja continuar? (S ou N)`);
+        if (ask === "s" || ask === "S") {
+            keep = true;
+            keepAsking = false;
+        } else if(ask === "n" || ask === "N"){
+            keep = false;
+            keepAsking = false;
+        } else {
+            alert(`opção inválida.`);
+        }
+    } while (keepAsking);
+} while (keep)
+
