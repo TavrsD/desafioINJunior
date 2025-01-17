4a0a05a4e96292546e358b778752c867f1debe94
Daniel <dtavares@id.uff.br>
2025-01-17 00:10:49 -0300

    feat:le coeficientes e calcula descriminante

--- exerc1.js
+++ exerc1.js
@@ -5,7 +5,7 @@ ax^2 + bx + c = 0
 
 O programa deve:
 1. Calcular o discriminante (Δ) pela fórmula:
-Δ = b2 − 4ac
+Δ = b² − 4ac
 
 2. Avaliar o valor de Δ para determinar o número de raízes reais:
 ○ Se Δ > 0, exibir as duas raízes reais e distintas.
@@ -15,6 +15,19 @@ O programa deve:
 3. Usar a fórmula de Bhaskara para calcular as raízes (caso existam)
 e imprimir na tela tais raízes:
 
-    x1 =        x2 =
+    x1 = (-b +sqrt(b²-4ac)) / 2a       x2 = (-b -sqrt(b²-4ac)) / 2a
 */
-console.log('Hello World!');
+
+alert("alcula raízes de funções quadráticas no formato ax² + bx + c = 0");
+
+const coefA = parseFloat(prompt("Escreva o coeficiente 'a':"));
+const coefB = parseFloat(prompt("Escreva o coeficiente 'b':"));
+const coefC = parseFloat(prompt("Escreva o coeficiente 'c':"));
+
+if (coefA === 0) {
+    alert("O coeficiente 'a' não pode ser zero.");
+} else if (isNaN(coefA) || isNaN(coefB)|| isNaN(coefC)) {
+    alert("Insira apenas números.");
+} else {
+    const descriminante = coefB^2 - (4*coefA*coefC);
+}
