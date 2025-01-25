e02d5de11e412cd21fe683425b446588a6d62ee3
Daniel <dtavares@id.uff.br>
2025-01-25 18:35:51 -0300

    feat: exercicio 9 adicionado

---
+++ exerc9.js
@@ -0,0 +1,90 @@
+/*
+Escreva uma função chamada authors que retorne um array com os
+nomes de todos os autores presentes no objeto booksByCategory,
+desconsiderando autores repetidos.
+*/
+
+function authors(booksArray){
+  let response = []
+  return response;
+}
+
+const booksByCategory = [
+  {
+    category: "Romance",
+    books: [
+      {
+      title: "O Sol Também é Uma Estrela",
+      author: "Nicola Yoon",
+      },
+      {
+      title: "O Noivo da Minha Melhor Amiga",
+      author: "Emily Giffin",
+      
+      },
+      {
+      title: "Orgulho e Preconceito",
+      author: "Jane Austen",
+      },
+    ],
+  },
+  {
+    category: "Fantasia",
+    books: [
+      {
+      title: "Harry Potter e a Pedra Filosofal",
+      author: "J.K. Rowling",
+      },
+      {
+      title: "O Hobbit",
+      author: "J.R.R Tolkien",
+      },
+      {
+      title: "Eragon",
+      author: "Christopher Paolini",
+      },
+      {
+      title: "O Nome do Vento",
+      author: "Patrick Rothfuss",  
+      }
+    ]
+  },
+  {
+    category: "Suspense",
+    books: [
+      {
+      title: "A Garota no Trem",
+      author: "Paula Hawkins",  
+      },
+      {
+      title: "O Silêncio dos Inicentes",
+      author: "Thomas Harris",  
+      }
+    ]
+  },
+  {
+    category: "Ficção Cientifica",
+    books: [
+      {
+      title: "Duna",
+      author: "Frank Herbert",
+      },
+      {
+      title: "Neuromancer",
+      author: "William Gibson",
+      },
+      {
+      title: "Fundação",
+      author: "Isaac Azimov",
+      },
+      {
+      title: "1984",
+      author: "George Orwell",
+      },
+      {
+      title: "O Fim da Infância",
+      author: "Arthur C. Clarlke",
+      }
+    ]
+  }
+]
